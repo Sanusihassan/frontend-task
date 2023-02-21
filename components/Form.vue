cb8273c3df19e57f6c17c07332f524f28dfd2bdc
Sanusi Hassan <sanusihassan122@gmail.com>
2023-02-21 17:10:21 +0200

    added a submit button

--- components/Form.vue
+++ components/Form.vue
@@ -28,7 +28,7 @@
         </option>
       </select>
       <label for="process-type" class="label"
-        >Sub Category <span class="text-red-400">*</span>
+        >process type<span class="text-red-400">*</span>
       </label>
       <select name="process-type" value="Other" class="menu">
         <option value="Other">Other</option>
@@ -53,6 +53,12 @@
         <option value="b">b</option>
         <option value="c">c</option>
       </select>
+      <button
+        type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
     </form>
   </div>
 </template>
